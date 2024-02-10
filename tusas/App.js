a9a28988fc2a9a6bc57c5d56cef10dfad7a68ef8
elifkurtay <elif.kurtay00@gmail.com>
2024-02-10 12:52:57 +0300

    ring data

--- tusas/App.js
+++ tusas/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {TEXTS} from "./constants";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{TEXTS.title}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ tusas/constants.js
@@ -0,0 +1,5 @@
+export const TEXTS = {
+    title: "TUSAŞ Ring",
+    version: "Ring bilgileri en son 10.02.2024 tarihinde güncellenmiştir.",
+    next: "Sıradaki ring saati: ",
+}
